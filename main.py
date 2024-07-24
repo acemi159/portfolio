fb22f7116540d2eb3c9c8f8be353c0672cb397c4
acemi159 <61695430+acemi159@users.noreply.github.com>
2024-07-24 14:14:42 +0200

    Add description on the new columns

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import streamlit as st
 
 
-st.set_page_config(layout="wide")
+#st.set_page_config(layout="wide")
 
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("data/images/Cem.jpeg", width=600)
+    st.image("data/images/Cem.jpeg")
     
 with col2:
     st.title("Cem Akgul")
@@ -15,3 +15,8 @@ with col2:
     AI projects mainly in the domain of Computer Vision. I graduated in 2020 with a degree of Photonics Engineering at Mechatronics Faculty in WUT.
     """
     st.info(content)
+    
+content2 = """
+Below you an find some of the apps I have built in Python. Feel free to contact me!
+"""
+st.write(content2)
